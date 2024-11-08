e45783312e40151b2b9e788e2e1140879239fdb4
Hishem <hishemok@uio.no>
2024-11-08 13:14:32 +0100

    removed a tiny comment

--- galerkin.py
+++ galerkin.py
@@ -6,7 +6,6 @@ from numpy.polynomial import Legendre as Leg
 from numpy.polynomial import Chebyshev as Cheb
 
 x = sp.Symbol('x')
-#anna97pha@hotmail.com
 
 def map_reference_domain(x, d, r):
     return r[0] + (r[1]-r[0])*(x-d[0])/(d[1]-d[0])
